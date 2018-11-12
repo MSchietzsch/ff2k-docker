56fb803c1a705c2ba949de7c8003d8f347a8ca9f
Michael Schietzsch <schietzsch@googlemail.com>
2018-11-12 16:09:09 +0100

    switched from mysql to pgsql

--- django-site/django-site/ff2k/settings.py
+++ django-site/django-site/ff2k/settings.py
@@ -130,12 +130,12 @@ elif ENVIRONMENT == "docker_compose":
 
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.mysql',
+            'ENGINE': 'django.db.backends.postgresql',
             'NAME': config('DB_NAME'),
             'USER': config('DB_USER'),
             'PASSWORD': config('DB_PASSWORD'),
             'HOST': config('DB_HOST'),
-            'PORT': '3306',
+            'PORT': config('DB_PORT'),
         }
     }
 
