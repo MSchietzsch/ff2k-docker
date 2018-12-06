6575218a06e4fdc2e1657d4f65cba60222cde4b1
Michael Schietzsch <michael.schietzsch@consorsfinanz.de>
2018-12-06 16:53:07 +0100

    added ideas and todo for tests

--- django-site/django-site/ff2ksite/tests.py
+++ django-site/django-site/ff2ksite/tests.py
@@ -2,13 +2,10 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 from ff2ksite.models import Story, Chapter, Tags, Fandom, Categories, Profile
 
-
 class FF2kTestCase(TestCase):
+    fixtures = ['sites.json', 'users.json', 'fandoms.json', 'data.json'] 
     def setUp(self):
-        self.user = User.objects.create_user(username='testuser', password='12345')
-        self.fandom = Fandom.objects.create(fandom_name='Harry Potter', fandom_descr='Harry Potter fandom refers to the community of fans of the Harry Potter books and movies who participate in entertainment activities that revolve around the series, such as reading and writing fan fiction, creating and soliciting fan art...', fandom_short='HP')
+        self.user = User.objects.create_user(username='Testinator', password='mE4OvEmXT7rT5OZQTet00eQB', email='testinator@fail4free.de')
 
-
-    def test_animals_can_speak(self):
-        login = self.client.login(username='testuser', password='12345')
-
+    def test_login(self):
+        login = self.client.login(username='Testinator', password='mE4OvEmXT7rT5OZQTet00eQB')
